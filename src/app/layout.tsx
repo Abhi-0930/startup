ad7fd50cbdc6d0d9761bcd2132aa983b9f247f01
Abhishek Jujjuvarapu <abhishek.j3094@gmail.com>
2026-03-31 21:42:48 +0530

    created the boilerplate template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,17 @@ const geistMono = Geist_Mono({
 const satoshi = localFont({
   src: [
     {
-      path: "../../public/fonts/Satoshi-Regular.woff2",
+      path: "../fonts/Satoshi-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Satoshi-Bold.woff2",
+      path: "../fonts/Satoshi-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Satoshi-Light.woff2",
+      path: "../fonts/Satoshi-Light.woff2",
       weight: "300",
       style: "normal",
     },
