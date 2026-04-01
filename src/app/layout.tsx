3001f3a60a711a611939661fc4ae42ea29cd6548
Abhishek Jujjuvarapu <abhishek.j3094@gmail.com>
2026-04-01 20:39:26 +0530

    created the boilerplate template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 import localFont from "next/font/local";
 
 const satoshi = localFont({
